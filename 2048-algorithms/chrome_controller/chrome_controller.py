50c4f76ce46d2ff481f12f404f5f92c0d93a4a8d
Viljami Latvala <viljami.latvala@helsinki.fi>
2022-09-08 12:30:05 +0300

    Begun creating controller to play 2048 in browser

---
+++ 2048-algorithms/chrome_controller/chrome_controller.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class BrowserGame:
+    def __init__(self):
+        self.url = 'https://play2048.co/'
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    
+    def start_game(self):
+        self.driver.get(self.url)
+        self.driver.find_element(By.ID, 'ez-accept-all').click()
+        self.game = self.driver.find_element(By.TAG_NAME,'body')
+
+    def move_up(self):
+        self.game.send_keys(Keys.ARROW_UP)
+    
+    def move_down(self):
+        self.game.send_keys(Keys.ARROW_DOWN)
+
+    def move_left(self):
+        self.game.send_keys(Keys.ARROW_LEFT)
+
+    def move_right(self):
+        self.game.send_keys(Keys.ARROW_RIGHT)
+
+    def read_grid(self):
+        grid = [
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0]
+        ]
+
+        tile_container = self.driver.find_element(By.CLASS_NAME, 'tile-container')
+        tiles = tile_container.find_elements(By.CLASS_NAME, 'tile')
+        for tile in tiles:
+            tile_classes = tile.get_attribute('class')
+            position_prefix = 'tile-position-'
+            tile_pos = tile_classes.find(position_prefix)
+            pos_str_start = tile_pos + len(position_prefix)
+            pos_str_end = pos_str_start + 3
+            pos_str = tile_classes[pos_str_start:pos_str_end]
+            tile_col = int(pos_str[0]) - 1
+            tile_row = int(pos_str[2]) - 1
+            tile_val = int(tile.text)
+            print(f'col: {tile_col} - row {tile_row} - value: {tile_val}')
+            grid[tile_row][tile_col] = tile_val
+
+        return grid
+
+browserGame = BrowserGame()
+browserGame.start_game()
+browserGame.move_down()
+grid = browserGame.read_grid()
+print(grid) 
--- documentation/weekly_progress_reports/week1.md
+++ documentation/weekly_progress_reports/week1.md
@@ -1,7 +1,14 @@
 # Weekly progress report - Week 1
 
 ## What has been done this week?
-This week was used to select the topic for the course project and do preliminary research on what implementing it would include. I had checked the topic ideas list on course page beforehand but I really found the opening lecture to be helpful in deciding on a topic stroke a balance between interesting, challenging and attainable.
+This week was used to
+
+-Select the topic for the course project and do preliminary research on what implementing it would include. I had checked the topic ideas list on course page beforehand but I really found the opening lecture to be helpful in deciding on a topic stroke a balance between interesting, challenging and attainable.
+
+-Create a Git repository for the project, setting up documentation structure and other administratory stuff for the course.
+
+-Writing up the definition document of the project.
+
 
 ## How has the program progressed?
 
@@ -13,3 +20,9 @@ I am a returning student, so there has been some overhead to re-familiarize myse
 ## What will be done next?
 
 ## Other feedback or questions to the course instructor
+
+## Hours worked
+| Day      | Hours worked | Description |
+|----------|--------------|-------------|
+| WED 7.9. | 2            | Opening Lecture |
+| THU 8.9. | 4            | Topic selection & background research, project |setup, documentation |
